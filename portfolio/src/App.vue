ea9cb1abb9680854dddf8c04d08ff62ac0ce9e25
NoirAbhinav <abhinavbbps2000@gmail.com>
2024-01-04 23:42:43 +0530

    feat: Started Intro Page

--- portfolio/src/App.vue
+++ portfolio/src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <!-- Add the Intro component -->
     <IntroPage />
+    <AboutMe />
   </div>
 </template>
 
 <script>
 import IntroPage from './components/IntroPage.vue';
+import AboutMe from './components/AboutMe.vue';
 
 export default {
   name: 'App',
   components: {
     IntroPage,
+    AboutMe,
   },
 };
 </script>
@@ -22,7 +25,9 @@ body {
   margin: 0;
   font-family: Arial, sans-serif;
 }
+
 #app {
+  @import "@/styles/global.scss";
   /* Add styles for the main app container */
 }
 </style>
---
+++ portfolio/src/components/AboutMe.vue
@@ -0,0 +1,51 @@
+<template>
+    <AnimationCommon name="fadeDown" :duration="1" class="about">
+        <div class="about-me">
+            <h3>About Me</h3>
+            <p class="description">{{ description }}</p>
+        </div>
+    </AnimationCommon>
+</template>
+  
+<script>
+import 'vue2-animate/dist/vue2-animate.min.css';
+import AnimationCommon from './AnimationCommon.vue';
+
+export default {
+    data() {
+        return {
+            description: "I'm a passionate developer excited about creating meaningful projects!",
+            isScrolledIn: false,
+        };
+    },
+    components: { AnimationCommon }
+};
+</script>
+  
+<style scoped>
+.about {
+    height: 20vh;
+}
+
+.about-me {
+    text-align: center;
+
+}
+
+.about-me h2 {
+    font-size: 28px;
+    margin-bottom: 10px;
+    color: #000;
+}
+
+.about-me .description {
+    font-size: 16px;
+    color: #000;
+}
+
+/* Apply the animation classes from vue2-animate */
+.fadeRight {
+    animation-duration: 1s;
+}
+</style>
+  
